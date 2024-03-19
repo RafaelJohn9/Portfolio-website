29e3f29db9516c028e5337f6b1a920046714db14
RafaelJohn9 <rafaeljohb@gmail.com>
2024-03-19 20:45:52 +0300

    connected container mysql services with external mysql

--- backend/user/install.sh
+++ backend/user/install.sh
@@ -3,27 +3,21 @@
 
 # Define variables
 DOCKER_IMAGE="user_backend_api"
-EXTERNAL_MYSQL_HOST="localhost"
+EXTERNAL_MYSQL_HOST="127.0.0.1"
 EXTERNAL_MYSQL_USER="user"
 MYSQL_ROOT_PASSWORD="password"
 MYSQL_DATABASE="PortfolioDB"
 
+cat models/engine/db.sql | sudo mysql -u root
 
 # Connect container's MySQL db to the external MySQL
-# docker run -it --rm -p 5000:5000 "$DOCKER_IMAGE" /bin/bash -c \
-#	"service mariadb start && \
-#	mysql -h'$EXTERNAL_MYSQL_HOST' \
-#       -u'$EXTERNAL_MYSQL_USER' \
-#       '$MYSQL_DATABASE'"
-
 docker run -it --rm -p 5000:5000 "$DOCKER_IMAGE" /bin/bash -c \
-  "service mariadb start && \
-   mysql -h'$EXTERNAL_MYSQL_HOST' -u'root'\
-        -e \"CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;\" && \
+  "service mariadb start && 
+  cat models/engine/db.sql | mysql -uroot &&\
+  cat slave.sql | mysql -uroot &&\
+
    mysql -h'$EXTERNAL_MYSQL_HOST' -u'$EXTERNAL_MYSQL_USER' -p'$MYSQL_ROOT_PASSWORD' \
-        -e \"CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; \
-        GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$EXTERNAL_MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';\" && \
-   /usr/bin/cat models/engine/db.sql | mysql -u root
+        -e \"USE $MYSQL_DATABASE;\" && \
+
    source .api_keys.env && \
    exec /bin/bash"
-
---
+++ backend/user/slave.sql
@@ -0,0 +1,10 @@
+CHANGE MASTER TO
+  MASTER_HOST='external_mysql_host',
+  MASTER_PORT=3306,
+  MASTER_USER='replication_user',
+  MASTER_PASSWORD='replication_password',
+  MASTER_LOG_FILE='mysql-bin.00359',  -- Use SHOW MASTER STATUS on the master to get this value
+  MASTER_LOG_POS=4757;                   -- Use SHOW MASTER STATUS on the master to get this value
+
+START SLAVE;
+SET GLOBAL read_only = OFF;
